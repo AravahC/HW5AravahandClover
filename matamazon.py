e5d570c8e3a3ffa1f13c0b86967cfc65511ddacc
AravahC <willowpawleaf@gmail.com>
2026-07-22 23:16:56 +0300

    Fixed should be last error

--- matamazon.py
+++ matamazon.py
@@ -761,10 +761,9 @@ def main():
         # If no output file was given,
         # print the JSON to the terminal.
         system.export_orders(sys.stdout)
-
+import traceback
 if __name__ == "__main__":
     try:
         main()
     except Exception:
-      print("The matamazon script has encountered an error")
-      sys.exit(0)
+        traceback.print_exc()
